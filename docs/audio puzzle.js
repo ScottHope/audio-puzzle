ef7c1bb45aca0d58b74e701fb6b7df432fbdf6e6
ScottHope <hughegreen@btinternet.com>
2021-06-12 17:46:14 +0100

    edit all files

--- docs/audio puzzle.js
+++ docs/audio puzzle.js
@@ -1,3 +1,3 @@
-let audio = document.querySelector("#audio");
+let audio = document.querySelector(".audio");
 let chkbx = document.querySelector("#checkbox");
-chkbk.addEventListener("input", function(){chkbx.checked === true ? audio.loop === true : audio.loop ===false});
+chkbx.addEventListener("input", function(){chkbx.checked === true ? audio.loop = true : audio.loop = false});
